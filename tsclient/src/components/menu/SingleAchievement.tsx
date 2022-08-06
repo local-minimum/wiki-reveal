3e84cd7ec49926d74f76726bdffcac0d485816e1
Martin Zackrisson <martin.zackrisson@molflow.com>
2022-08-06 11:07:57 +0200

    Manage tooltip
    Allow mobile users to click to see info about achievement

--- tsclient/src/components/menu/SingleAchievement.tsx
+++ tsclient/src/components/menu/SingleAchievement.tsx
@@ -1,8 +1,8 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
+import { faCircleQuestion, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  Card, CardContent, Tooltip, Typography,
+  Card, CardContent, IconButton, Tooltip, Typography, useMediaQuery, useTheme,
 } from '@mui/material';
 import * as React from 'react';
 
@@ -37,14 +37,34 @@ function HiddenAchievement(): JSX.Element {
 export function SingleAchievement({
   gameId, icon, color, title, description,
 }: SingleAchievementProps): JSX.Element {
+  const theme = useTheme();
+  const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
+  const [openTooltip, setOpenTooltip] = React.useState<boolean>(false);
+  const handleOpenTooltip = React.useCallback(() => setOpenTooltip(true), []);
+  const handleCloseTooltip = React.useCallback(() => setOpenTooltip(false), []);
+
   if (gameId === undefined) {
     return <HiddenAchievement />;
   }
 
   return (
     <Card sx={{ margin: 0.5 }}>
-      <Tooltip title={description}>
+      <Tooltip
+        title={description}
+        open={openTooltip}
+        onOpen={handleOpenTooltip}
+        onClose={handleCloseTooltip}
+      >
         <CardContent sx={{ color, backgroundColor: '#222' }}>
+          {isSmall && (
+            <IconButton
+              onClick={handleOpenTooltip}
+              size="small"
+              sx={{ float: 'right', marginRight: -1, color }}
+            >
+              <FontAwesomeIcon icon={faInfoCircle} />
+            </IconButton>
+          )}
           <div>
             <FontAwesomeIcon icon={icon} size="3x" />
           </div>
