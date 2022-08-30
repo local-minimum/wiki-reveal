23f6a6089c7cb15ed1739dcb241088ff048316cb
Martin Zackrisson <martin.zackrisson@molflow.com>
2022-08-30 19:16:51 +0200

    Fix mode switch bug

--- tsclient/src/hooks/useRevealedPage.ts
+++ tsclient/src/hooks/useRevealedPage.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Guess } from '../components/Guess';
 import { Page } from '../types/wiki';
 import { unmaskPage } from '../utils/wiki';
@@ -6,33 +6,42 @@ import usePrevious from './usePrevious';
 
 const emptyPage: Page = { title: [], summary: [], sections: [] };
 
-function useArrDiff(current: Guess[]): Guess[] {
-  const prev = usePrevious(current) ?? [];
-  return current.filter(([lex], idx) => lex !== prev[idx]?.[0]);
-}
-
 function useRevealedPage(
   page: Page | undefined,
   guesses: Guess[],
 ): Page {
   const prevPage = usePrevious(page);
-  const pageChange = prevPage !== page && prevPage !== undefined;
+  const pageChange = prevPage !== page;
   const [revealed, setRevealed] = useState<Page>(page ?? emptyPage);
-  const newGuesses = useArrDiff(guesses);
+  const guessesRef = useRef<Guess[]>([]);
 
   useEffect(() => {
-    setRevealed(page ?? emptyPage);
-  }, [page]);
-
-  useEffect(() => {
-    if (pageChange || (newGuesses.length === 0)) return;
+    if (pageChange) {
+      setRevealed(page ?? emptyPage);
+      guessesRef.current = [];
+      return;
+    }
+    const divergenceIdx = guesses.findIndex(([lex], idx) => lex !== guessesRef.current[idx]?.[0]);
+    if (guessesRef.current.length > divergenceIdx + 1) {
+      if (divergenceIdx >= 0 || guesses.length !== guessesRef.current.length) {
+        setRevealed(page ?? emptyPage);
+        guessesRef.current = [];
+      }
+      return;
+    }
+    if (divergenceIdx < 0) {
+      return;
+    }
+    const newGuesses = guesses.slice(divergenceIdx);
+    guessesRef.current = guesses;
+    if (newGuesses.length === 0) return;
 
     const updated = unmaskPage(
       revealed,
       Object.fromEntries(newGuesses.map(([lex]) => [lex, true])),
     );
     setRevealed(updated);
-  }, [newGuesses, pageChange, revealed]);
+  }, [guesses, pageChange, revealed, page]);
 
   return revealed;
 }
